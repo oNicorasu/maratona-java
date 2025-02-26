2e7f3068c4a9d7c9b5f16ec282830635abb53f40
oNicorasu <nicolasmfs9@gmail.com>
2025-02-26 19:12:44 -0300

    Arquivos iniciais de Estudo

---
+++ src/estudos/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,11 @@
+package estudos.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+
+public class Aula01OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+    }
+}
---
+++ src/estudos/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package estudos.maratonajava.introducao;
+
+/**
+ * Tipos Primitivos: int, float, double, long, byte, short, boolean, char
+ */
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 10;
+        long bigNumber = 100000000;
+        float salary = 1853;
+        double bonus = 50000;
+        char genre = 'M';
+        byte ageInByte = 20;
+        short ageInShort = 20;
+        boolean ageInBoolean = true;
+        boolean ageInBoolean2 = false;
+        char genreInChar = 65;
+
+        System.out.println("Meu primo tem " + age + " anos");
+        System.out.println("Meu primo tem " + bigNumber + " anos");
+        System.out.println(genreInChar);
+    }
+}
