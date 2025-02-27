6d518dbec6505bb8a3b7362d16f09fc3797fff79
oNicorasu <nicolasmfs9@gmail.com>
2025-02-27 00:29:28 -0300

    feat: Aula 02 e 03 concluídas

--- src/estudos/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/estudos/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -6,9 +6,9 @@ package estudos.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        int age = 10;
-        long bigNumber = 100000000;
-        float salary = 1853;
+        int age = (int) 10000000000L;
+        long bigNumber = 100000000L;
+        float salary = 1853F;
         double bonus = 50000;
         char genre = 'M';
         byte ageInByte = 20;
@@ -16,9 +16,11 @@ public class Aula02TiposPrimitivos {
         boolean ageInBoolean = true;
         boolean ageInBoolean2 = false;
         char genreInChar = 65;
+        String genreInString = "Masculino";
 
         System.out.println("Meu primo tem " + age + " anos");
         System.out.println("Meu primo tem " + bigNumber + " anos");
         System.out.println(genreInChar);
+        System.out.println(genreInString);
     }
 }
---
+++ src/estudos/maratonajava/introducao/Aula03ExercicioTiposPrimitivos.java
@@ -0,0 +1,11 @@
+package estudos.maratonajava.introducao;
+
+public class Aula03ExercicioTiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Maria";
+        String endereço = "Rua do Mario, 171";
+        float salario = 1500.00F;
+        String data = "26/02/2025";
+        System.out.printf("Eu %s, morando no endereço %s, confirmo que recebi o salário de %f, na data %s",nome,endereço,salario,data);
+    }
+}
